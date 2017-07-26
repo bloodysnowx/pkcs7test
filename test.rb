891758ade796d81b24992948015ac644bd981dd7
Atsushi Iwasa <a-iwasa@m3.com>
2017-07-26 18:47:57 +0900

    fix ruby

--- test.rb
+++ test.rb
@@ -1,7 +1,13 @@
 require 'openssl'
 
-cert_data = File.read('./test.p12')
+orig_text = 'data'
+cert_data = File.read('./test2.p12')
 p12 = OpenSSL::PKCS12.new(cert_data, 'test')
-signature = OpenSSL::PKCS7::sign(p12.certificate, p12.key, 'data', nil, OpenSSL::PKCS7::BINARY | OpenSSL::PKCS7::DETACHED)
+signed_data = OpenSSL::PKCS7::sign(p12.certificate, p12.key, orig_text, p12.ca_certs, OpenSSL::PKCS7::BINARY | OpenSSL::PKCS7::DETACHED)
 
-File.open('./signature', 'wb+') do |file| file.write(signature.to_der) end
+File.open('./ruby.signed', 'wb+') do |file| file.write(signed_data.to_der) end
+pkcs7 = File.open('./ruby.signed') do |file| OpenSSL::PKCS7.new(file.read) end
+
+store = OpenSSL::X509::Store.new
+p12.ca_certs.each{|ca| store.add_cert(ca)}
+verified = pkcs7.verify(nil, store, orig_text, OpenSSL::PKCS7::NOVERIFY)
